27e19b20d9e3aefa8643fed22d9c7a69296da346
samedkoksal <sametkoksal1996@gmail.com>
2025-04-08 19:56:44 +0300

    İlk commit: Proje dosyaları eklendi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+        <div className="min-h-screen bg-blue-300 flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-purple-600 ">Tailwind Sorunsuz Çalışıyor 🎉</h1>
+        </div>
+    </>
+  );
+}
+
+export default App;
